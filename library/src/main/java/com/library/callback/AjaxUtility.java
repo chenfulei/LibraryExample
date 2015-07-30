c98a4649d84bdc7da237e46de44adffe46a3941c
chen_fulei <cfulei@gmail.com>
2015-07-30 17:26:30 +0800

    add file

--- library/src/main/java/com/library/callback/AjaxUtility.java
+++ library/src/main/java/com/library/callback/AjaxUtility.java
@@ -1,10 +1,15 @@
 package com.library.callback;
 
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.os.Looper;
 
 import com.library.utils.Debug;
 
 import java.io.Closeable;
+import java.lang.reflect.Method;
+
+import static com.library.callback.AjaxUtility.postAsync;
 
 /**
  * ajax 请求的所有公共方法
@@ -12,6 +17,140 @@ import java.io.Closeable;
  * Created by chen_fulei on 2015/7/28.
  */
 public class AjaxUtility {
+
+
+    /**
+     * 对象反射
+     * @param object 对象
+     * @param callback 对象方法
+     * @param fallback 是否返回
+     * @param cls 返回的类型【JSON.class , String.class ....】
+     * @param cls2
+     * @param params 返回的参数
+     * @return
+     * @throws Exception
+     */
+    public static Object invokeMethod(Object object , String callback , boolean fallback , Class<?>[] cls , Class<?> cls2 , Object... params){
+        if (object == null || callback == null) return null;
+
+        Method method = null;
+
+        try {
+            if (cls == null) cls = new Class[0];
+
+            method = object.getClass().getMethod(callback , cls);
+            return method.invoke(object , params);
+        }catch (Exception e){
+            e.printStackTrace();
+            Debug.Log(e);
+        }
+
+        try {
+
+            if (fallback){
+
+                if (cls2 == null){
+                    method = object.getClass().getMethod(callback);
+                    return method.invoke(object);
+                }else{
+                    method = object.getClass().getMethod(callback , cls2);
+                    return method.invoke(object , params);
+                }
+
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+            Debug.Log(e);
+        }
+
+        return null;
+    }
+    public static Object invokeMethod(Object object , String callback , boolean fallback , Class<?>[] cls , Object... params){
+        return invokeMethod(object , callback , fallback , cls , null , params);
+    }
+
+
+    //创建异步线程（不在主线程）
+    private static Handler handler;
+    public static Handler getHandler(){
+        if (handler == null){
+            handler = new Handler(Looper.getMainLooper());
+        }
+
+        return  handler;
+    }
+
+    /**
+     * 执行线程请求
+     * @param runnable
+     */
+    public static void post(Runnable runnable ){
+        getHandler().post(runnable);
+    }
+
+    /**
+     * 执行异步反射
+     * @param object
+     * @param method
+     * @param cls
+     * @param params
+     */
+    public static void post(final Object object , final String method , final Class<?>[] cls , final Object... params){
+        post(new Runnable() {
+
+            @Override
+            public void run() {
+                //反射
+                AjaxUtility.invokeMethod(object, method, false, cls, params);
+            }
+        });
+    }
+    public static void post(Object object , String method){
+        post(object , method , new Class[0]);
+    }
+
+    /**
+     * 同步执行
+     * @param runnable
+     */
+    public static void postAsync(final Runnable runnable){
+        AsyncTask<Void , Void , String> task = new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                try {
+                    runnable.run();
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                    Debug.Log(e);
+                }
+
+                return null;
+            }
+        };
+
+        task.execute();
+    }
+
+    /**
+     * 执行反射
+     * @param object
+     * @param method
+     * @param cls
+     * @param params
+     */
+    public static void postAsync(final Object object , final String method , final Class<?>[] cls , final Object... params){
+        postAsync(new Runnable() {
+            @Override
+            public void run() {
+                AjaxUtility.invokeMethod(object, method, false, cls, params);
+            }
+        });
+    }
+    public static void postAsync(Object object , String method){
+        postAsync(object , method , new Class[0]);
+    }
 
 
     /**
@@ -23,6 +162,23 @@ public class AjaxUtility {
         long cId = Thread.currentThread().getId();
 
         return uiId == cId;
+    }
+
+    /**
+     * 注销线程
+     * @param runnable
+     */
+    public static void removePost(Runnable runnable){
+        getHandler().removeCallbacks(runnable);
+    }
+
+    /**
+     * 在规定时间内注销线程
+     * @param runnable
+     * @param delay
+     */
+    public static void postDelayed(Runnable runnable , long delay){
+        getHandler().removeCallbacks(runnable , delay);
     }
 
 
