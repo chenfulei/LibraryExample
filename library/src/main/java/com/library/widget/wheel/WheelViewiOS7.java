0e865856d2dff47bca4020cfb36304cd4555afd1
chen_fulei <cfulei@gmail.com>
2015-08-24 09:46:01 +0800

    add file

---
+++ library/src/main/java/com/library/widget/wheel/WheelViewiOS7.java
@@ -0,0 +1,26 @@
+package com.library.widget.wheel;
+
+import android.content.Context;
+import android.graphics.Color;
+
+import com.library.R;
+
+/**
+ *  -.- 类似IOS7选择器的模样
+ * @author zl.lin
+ *
+ */
+public class WheelViewiOS7 extends WheelView {
+
+	public WheelViewiOS7(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+		setCenterDrawable(R.drawable.ios7_wheel_val);
+		setWheelBackgroundColor(Color.WHITE);
+		int colors[] = { Color.TRANSPARENT, Color.TRANSPARENT,
+				Color.TRANSPARENT };
+		setTopShadowDrawable(colors);
+		setBottomShadowDrawable(colors);
+	}
+
+}
