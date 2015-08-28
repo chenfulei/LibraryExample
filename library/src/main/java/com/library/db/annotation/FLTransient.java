febe1d4a4ead1ffab41b4eeb28e82bd6101e3452
chen_fulei <cfulei@gmail.com>
2015-08-28 17:34:02 +0800

    add file

---
+++ library/src/main/java/com/library/db/annotation/FLTransient.java
@@ -0,0 +1,14 @@
+package com.library.db.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by chen_fulei on 2015/8/28.
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FLTransient {
+}
