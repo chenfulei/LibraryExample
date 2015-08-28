bb74ac4c8f4a36a4a23578905129db266d18f0ad
chen_fulei <cfulei@gmail.com>
2015-08-28 17:23:09 +0800

    add file

---
+++ library/src/main/java/com/library/db/annotation/FLCheck.java
@@ -0,0 +1,16 @@
+package com.library.db.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * Created by chen_fulei on 2015/8/28.
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FLCheck {
+    String value();
+}
---
+++ library/src/main/java/com/library/db/annotation/FLColumn.java
@@ -0,0 +1,18 @@
+package com.library.db.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by chen_fulei on 2015/8/28.
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FLColumn {
+
+    String column() default "";
+
+    String defaultValue() default "";
+}
